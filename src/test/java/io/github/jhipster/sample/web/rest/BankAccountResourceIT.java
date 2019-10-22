4c98f68ffa7c8fa1fe70e07265adb5a601028652
niravassar <assarn@objectcomputing.com>
2019-10-22 16:25:11 -0500

    equals test

--- src/test/java/io/github/jhipster/sample/web/rest/BankAccountResourceIT.java
+++ src/test/java/io/github/jhipster/sample/web/rest/BankAccountResourceIT.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Integration tests for the {@Link BankAccountResource} REST controller.
@@ -238,18 +237,17 @@ public class BankAccountResourceIT {
 //        assertThat(bankAccountList).hasSize(databaseSizeBeforeDelete - 1);
 //    }
 //
-//    @Test
-//    @Transactional
-//    public void equalsVerifier() throws Exception {
-//        TestUtil.equalsVerifier(BankAccount.class);
-//        BankAccount bankAccount1 = new BankAccount();
-//        bankAccount1.setId(1L);
-//        BankAccount bankAccount2 = new BankAccount();
-//        bankAccount2.setId(bankAccount1.getId());
-//        assertThat(bankAccount1).isEqualTo(bankAccount2);
-//        bankAccount2.setId(2L);
-//        assertThat(bankAccount1).isNotEqualTo(bankAccount2);
-//        bankAccount1.setId(null);
-//        assertThat(bankAccount1).isNotEqualTo(bankAccount2);
-//    }
+    @Test
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(BankAccount.class);
+        BankAccount bankAccount1 = new BankAccount();
+        bankAccount1.setId(1L);
+        BankAccount bankAccount2 = new BankAccount();
+        bankAccount2.setId(bankAccount1.getId());
+        assertThat(bankAccount1).isEqualTo(bankAccount2);
+        bankAccount2.setId(2L);
+        assertThat(bankAccount1).isNotEqualTo(bankAccount2);
+        bankAccount1.setId(null);
+        assertThat(bankAccount1).isNotEqualTo(bankAccount2);
+    }
 }
